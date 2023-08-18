d5b427181c3fe1968420388da05539c752a6a14c
SK ASIF HOSSAIN <109328835+asifhossain-git@users.noreply.github.com>
2023-08-18 18:44:27 +0530

    Update q8.R

--- Lab2/q8.R
+++ Lab2/q8.R
@@ -1,3 +1,5 @@
+#Write an R-script to create a 3*3 matrix and update that matrix by adding 4 to each and every element, also display the updated matrix.
+
 matrix <- matrix(1:9, nrow = 3, ncol = 3)
 
 updated_matrix <- matrix + 4
