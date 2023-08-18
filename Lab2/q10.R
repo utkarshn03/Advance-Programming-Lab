fae6bb6ef4e088eb01f2c5e908d383341242c135
SK ASIF HOSSAIN <109328835+asifhossain-git@users.noreply.github.com>
2023-08-18 18:45:39 +0530

    Update q10.R

--- Lab2/q10.R
+++ Lab2/q10.R
@@ -1,3 +1,5 @@
+#Write an R-script to check the given matrix is symmetric matrix or not?
+
 matrix <- matrix(c(1, 2, 3, 2, 4, 5, 3, 5, 6), nrow = 3, ncol = 3)
 
 is_symmetric <- all(matrix == t(matrix))
